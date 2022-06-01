7f5b7fea95506934eb67bbb394ce8d97bbb81c01
AkankshaJadhav <akankshajadhav2999@gmail.com>
2022-06-01 11:44:34 +0530

    Executed: Printed the elements of the array

---
+++ src/PrintArrayElerments.java
@@ -0,0 +1,14 @@
+public class PrintArrayElerments {
+    public static void main(String[] args) {
+
+        //Initilize the array elements
+        int[] myList = {1, 2, 3, 3};
+
+        //Loop through the array
+        for (int i = 0; i < myList.length; i++) {
+
+            //Printing the array elements
+            System.out.println(myList[i] + " ");
+        }
+    }
+}
